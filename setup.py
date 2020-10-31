894b2646299dfa20eb00f294e4933141ca825667
Sachin Raja <sachinraja2349@gmail.com>
2020-10-31 09:15:45 -0700

    Initial commit

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(name="discordbot-dash-xCloudzx", 
+version="0.1",
+author="Sachin Raja",
+author_email="sachinraja2349@gmail.com",
+license="MIT",
+description="A Discord bot management package",
+long_description=long_description,
+long_description_content_type="text/markdown",
+url="https://github.com/xCloudzx/discordbot-dash",
+packages=find_packages(),
+classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+]
+)
