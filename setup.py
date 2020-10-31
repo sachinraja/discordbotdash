54b80411d68415e4c189c32240db5f9d528f06a4
Sachin Raja <sachinraja2349@gmail.com>
2020-10-31 09:49:08 -0700

    rename discordbot-dash to discordbotdash (ironic)

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setup(name="discordbot-dash-xCloudzx", 
-version="0.1",
+version="0.12",
 author="Sachin Raja",
 author_email="sachinraja2349@gmail.com",
 license="MIT",
 description="A Discord bot management package",
 long_description=long_description,
 long_description_content_type="text/markdown",
-url="https://github.com/xCloudzx/discordbot-dash",
+url="https://github.com/xCloudzx/discordbotdash",
 packages=find_packages(),
 classifiers=[
         "Programming Language :: Python :: 3",
